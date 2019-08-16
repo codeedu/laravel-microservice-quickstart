933f52b16b1a745749ff26ba539aa49f65d660a8
Luiz <argentinaluiz@gmail.com>
2019-08-15 21:49:16 -0300

    Improvements in build

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
+cp .env.example .env
 composer install
 php artisan key:generate
 php artisan migrate
