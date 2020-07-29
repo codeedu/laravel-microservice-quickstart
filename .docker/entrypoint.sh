8354deb3937ded4fa028781723d9e84429d55b45
Luiz Carlos <argentinaluiz@gmail.com>
2020-07-28 21:16:00 -0300

    Apply chown in /var/www

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
+chown -R www-data:www-data .
 composer install
 php artisan key:generate
 php artisan migrate
