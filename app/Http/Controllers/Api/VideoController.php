d7ad739275de2bd4db7f5c81ecf1f017c5bd0dfc
Leonardy Cardoso <leonardyrj@gmail.com>
2020-11-29 23:16:10 -0300

    Persistindo vídeos com Categorias e Gêneros

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 use App\Models\Video;
+use Illuminate\Http\Request;
 
 
 class VideoController extends BaseCrudController
@@ -16,8 +17,33 @@ class VideoController extends BaseCrudController
             'year_launched' => 'required|date_format:Y',
             'opened' => 'boolean',
             'rating' => 'required|in:'. implode(',',Video::RATING_LIST),
-            'duration' => 'required|integer'
+            'duration' => 'required|integer',
+            'categories_id' => 'required|array|exists:categories,id',
+            'genres_id' => 'required|array|exists:genres,id'
         ];
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $this->validate($request,$this->rulesStore());
+        /** @var Video $obj */
+        $obj = $this->model()::create($validatedData);
+        // O sync limpa tudo e faz de novo
+        $obj->categories()->sync($request->get('categories_id'));
+        $obj->genres()->sync($request->get('genres_id'));
+        $obj->refresh();
+
+        return $obj;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $obj = $this->findOrFail($id);
+        $validatedData = $this->validate($request, $this->rulesUpdate());
+        $obj->update($validatedData);
+        $obj->categories()->sync($request->get('categories_id'));
+        $obj->genres()->sync($request->get('genres_id'));
+        return $obj;
     }
 
     public function model()
