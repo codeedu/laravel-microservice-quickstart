a22ae9a642658cbb0f48fc914c641473b160954a
Leonardy Cardoso <leonardyrj@gmail.com>
2020-11-29 22:58:25 -0300

    Relacionando Videos com Categorias e Generos

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -31,4 +31,14 @@ class Video extends Model
     ];
 
     public $incrementing = false;
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class);
+    }
 }
