ffbb3fbc7338f90f83d5809471aa3f37d7f18be4
Leonardy Cardoso <leonardyrj@gmail.com>
2020-12-12 15:30:05 -0300

    Primeiro API Resource da Category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Resources\CateforyCollection;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 
 class CategoryController extends BaseCrudController
@@ -12,6 +14,19 @@ class CategoryController extends BaseCrudController
         'description' => 'nullable',
         'is_active' =>'boolean'
     ];
+
+    public function index()
+    {
+        $collection = parent::index();
+        return new CateforyCollection($collection);
+    }
+
+    public function show($id)
+    {
+        $obj = parent::show($id);
+        return new CategoryResource($obj);
+    }
+
 
     protected function model()
     {
--- database/seeds/VideoSeeder.php
+++ database/seeds/VideoSeeder.php
@@ -4,6 +4,11 @@ use Illuminate\Database\Seeder;
 
 class VideoSeeder extends Seeder
 {
+    private $allGenres;
+    private $relations = [
+        'genres_id' => [],
+        'categories' => []
+    ];
     /**
      * Run the database seeds.
      *
@@ -11,21 +16,51 @@ class VideoSeeder extends Seeder
      */
     public function run()
     {
-        $genres = \App\Models\Genre::all();
+        $dir = \Storage::getDriver()->getAdapter()->getPathPrefix();
+        \File::deleteDirectory($dir,true);
+        $self = $this;
+        $this->allGenres = \App\Models\Genre::all();
+        \Illuminate\Database\Eloquent\Model::reguard();
         factory(\App\Models\Video::class,100)
-            ->create()
-            ->each(function(\App\Models\Video $video) use($genres){
-               $subGenres = $genres->random(5)->load('categories');
-               $categoriesId = [];
-               foreach($subGenres as $genre)
-               {
-                   array_push($categoriesId, ...$genre->categories->pluck('id')->toArray());
-               }
-               $categoriesId = array_unique($categoriesId);
-               $video->categories()->attach($categoriesId);
-               $video->genres()->attach($subGenres->pluck('id')->toArray());
+            ->make()
+            ->each(function(\App\Models\Video $video) use ($self){
+               $self->fecthRelations();
+               \App\Models\Video::create(
+                   array_merge(
+                       $video->toArray(),
+                       [
+                           'thumb_file' => $self->getImageFile(),
+                           'banner_file' => $self->getImageFile(),
+                           'trailer_file' => $self->getVideoFile(),
+                           'video_file' => $self->getVideoFile()
+                       ],
+                       $this->relations
+                   )
+               );
             });
+        \Illuminate\Database\Eloquent\Model::unguard();
+    }
 
+    public function fecthRelations()
+    {
+        $subGenres = $this->allGenres->random(5)->load('categories');
+        $categoriesId = [];
+        foreach($subGenres as $genre){
+            array_push($categoriesId, ...$genre->categories->pluck('id')->toArray());
+        }
+        $categoriesId = array_unique($categoriesId);
+        $genresId = $subGenres->pluck('id')->toArray();
+        $this->relations['categories_id'] = $categoriesId;
+        $this->relations['genres_id'] = $genresId;
+    }
 
+    public function getImageFile()
+    {
+        return new \Illuminate\Http\UploadedFile(storage_path('faker/thumbs/image.png'),'image.png');
+    }
+
+    public function getVideoFile()
+    {
+        return new \Illuminate\Http\UploadedFile(storage_path('faker/videos/video.mp4'),'video.mp4');
     }
 }
