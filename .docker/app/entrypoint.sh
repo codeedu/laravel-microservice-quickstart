37245559f08889a359b52a1c91a4b517e0a61577
Victor Magalhães <victormagalhaessales@gmail.com>
2021-01-26 17:42:09 -0300

    rearranging folders

--- .docker/app/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
-chown -R www-data:www-data .
-composer install
-php artisan key:generate
-php artisan migrate
-
-php-fpm
