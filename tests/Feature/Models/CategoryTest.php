1e7f53c6f95d279f3d6226e2262e3fb07a551836
Jose Motta <josemotta@bampli.com>
2021-08-23 10:59:55 -0300

    iniciando com testes integracao DB

---
+++ tests/Feature/Models/CategoryTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testList()
+    {
+        // Category::create([
+        //     'name' => 'test1'
+        // ]);
+        factory(Category::class, 1)->create();
+        
+        $categories = Category::all();
+        $this->assertCount(1, $categories);
+        $categoryKey = array_keys($categories->first()->getAttributes());
+        $this->assertEqualsCanonicalizing(
+            [
+                'id',
+                'name',
+                'description',
+                'is_active',
+                'created_at',
+                'updated_at',
+                'deleted_at'
+            ],
+            $categoryKey
+        );
+    }
+}
--- tests/Unit/Models/CategoryTest.php
+++ tests/Unit/Models/CategoryTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Unit\Models;
 
 use App\Models\Category;
 use App\Models\Traits\Uuid;
