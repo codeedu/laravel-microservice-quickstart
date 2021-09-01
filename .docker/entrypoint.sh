2fa6fe2ea5bca2cca69338ef527e3dc5ba029e22
Jose Motta <josemotta@bampli.com>
2021-09-01 15:43:01 -0300

    change repo for ci build

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
+cp .env.example .env
+cp .env.testing.example .env.testing
+
 chown -R www-data:www-data .
 composer install
 php artisan key:generate
