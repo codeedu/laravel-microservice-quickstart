5298615df65023e235e8a8abbbac511f83e61a01
Jose Motta <josemotta@bampli.com>
2021-09-04 15:13:51 -0300

    finish cast_member and remove --prefer-dist --optimize-autoloader from cloud build

--- tests/Feature/Http/Controllers/Api/CastMemberControllerTest.php
+++ tests/Feature/Http/Controllers/Api/CastMemberControllerTest.php
@@ -32,4 +32,84 @@ class CastMemberControllerTest extends TestCase
             ->assertStatus(200)
             ->assertJson([$this->castMember->toArray()]);
     }
+
+    public function testShow()
+    {
+        $response = $this->get(route('cast_members.show', ['cast_member' => $this->castMember->id]));
+
+        $response
+            ->assertStatus(200)
+            ->assertJson($this->castMember->toArray());
+    }
+
+    public function testInvalidationData()
+    {
+        $data = [
+            'name' => '',
+            'type' => ''
+        ];
+        $this->assertInvalidationInStoreAction($data, 'required');
+        $this->assertInvalidationInUpdateAction($data, 'required');
+        
+        $data = [
+            'type' => 's'
+        ];
+        $this->assertInvalidationInStoreAction($data, 'in');
+        $this->assertInvalidationInUpdateAction($data, 'in');
+    }
+
+    public function testStore()
+    {
+        $data = [
+            [
+                'name' => 'test',
+                'type' => CastMember::TYPE_DIRECTOR
+            ],
+            [
+                'name' => 'test',
+                'type' => CastMember::TYPE_ACTOR
+            ]
+        ];
+        foreach ($data as $key => $value){
+            $response = $this->assertStore($value, $value + ['deleted_at' => null]);
+            $response->assertJsonStructure([
+            'created_at', 'updated_at'
+            ]);
+        }
+    }
+
+    public function testUpdate()
+    {
+        $data = [
+            'name' => 'test',
+            'type' => CastMember::TYPE_ACTOR
+        ];
+        $response = $this->assertUpdate($data, $data + ['deleted_at' => null]);
+        $response->assertJsonStructure([
+            'created_at', 'updated_at'
+        ]);
+    }
+
+    public function testDestroy()
+    {
+        $response = $this->json('DELETE', route('cast_members.destroy', ['cast_member' => $this->castMember->id]));
+        $response->assertStatus(204);
+        $this->assertNull(CastMember::find($this->castMember->id));
+        $this->assertNotNull(CastMember::withTrashed()->find($this->castMember->id));
+    }
+
+    protected function routeStore()
+    {
+        return route('cast_members.store');
+    }
+
+    protected function routeUpdate()
+    {
+        return route('cast_members.update', ['cast_member' => $this->castMember->id]);
+    }
+
+    protected function model()
+    {
+        return CastMember::class;
+    }
 }
