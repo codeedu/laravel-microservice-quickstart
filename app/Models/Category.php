c1f5244b2f54e9204e0ee8d7d2251c299fd09c57
Fabio Janio <fabiojaniolima@gmail.com>
2021-09-06 19:01:02 -0300

    chore: change auto-incremental ID to UUID

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes, Uuid;
+
     protected $fillable = ['name', 'description', 'is_active'];
+
+    protected $dates = ['deleted_at'];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
 }
--- database/migrations/2021_09_05_204202_create_categories_table.php
+++ database/migrations/2021_09_05_204202_create_categories_table.php
@@ -14,10 +14,11 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->text('description')->nullable();
             $table->boolean('is_active')->default(true);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
