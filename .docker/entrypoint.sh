d70a7ff6f265bf640cdd70be81e77e6ff37c3eb6
Péricles Luz <pericles.luz@gmail.com>
2021-12-11 17:44:47 -0300

    Correção em nome de arquivo e adição de variável

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 cp .env.example .env
-cp .env.example.testing .env.testing
+cp .env.testing.example .env.testing
 chown -R www-data:www-data .
 composer install
 php artisan key:generate
